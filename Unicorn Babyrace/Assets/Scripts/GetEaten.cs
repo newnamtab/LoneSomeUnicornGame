0063635c3a0ea468ae9d158f118578a078654ea8
Søren Hansen <newnamtab@yahoo.dk>
2017-02-09 19:13:00 +0100

    Eal Game Jame project

---
+++ Unicorn Babyrace/Assets/Scripts/GetEaten.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class GetEaten : MonoBehaviour
+{
+    public GameObject BabyBillede;
+    
+    public AudioClip clip;
+    public AudioClip clip2;
+    // Use this for initialization
+    void Start()
+    {
+        
+
+      //  gameObject.GetComponent<Animation>().Play("BloodSplatter");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Awake()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.name == "Player")
+        { 
+            gameObject.GetComponent<Animator>().SetTrigger("StartAnim");
+            Destroy(BabyBillede.gameObject);
+            AudioSource.PlayClipAtPoint(clip, transform.position);
+            AudioSource.PlayClipAtPoint(clip2, transform.position,10);
+
+
+            Destroy(gameObject.GetComponent<BoxCollider2D>());
+            
+
+            Destroy(gameObject,1);
+        }
+    }
+
+}
---
+++ Unicorn Babyrace/Assets/Scripts/scrip.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Threading;
+
+
+public class scrip : MonoBehaviour
+{
+
+
+    private int Speed = 15;
+    //private int movex = 5;
+    //private int movey = -5;
+    private Vector2 Unicorn;
+    Thread jumpThread;
+    bool Isjumping = false;
+    float lastTime;
+    //sleep jump
+    IEnumerator CheckJump()
+    {
+        Isjumping = true;
+        yield return new WaitForSeconds(1);
+        Isjumping = false;
+    }
+
+    
+    // Use this for initialization
+    void Start()
+    {
+        lastTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time >= lastTime + 0.1)
+        {
+            lastTime = Time.time;
+            if (Input.GetKey(KeyCode.D))
+            {
+                Unicorn.Set(Speed, 0);
+                transform.Translate(Unicorn);
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                Unicorn.Set(-Speed, 0);
+                transform.Translate(Unicorn);
+            }
+            
+
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+
+            if (Isjumping == false)
+            {
+                Unicorn.Set(0, 10);
+                transform.Translate(Unicorn);
+                StartCoroutine(CheckJump());
+
+            }
+
+        }
+
+
+    }
+    
+}
